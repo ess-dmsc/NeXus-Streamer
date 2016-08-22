267adfd204449ff8edfb73bed6ff23a8a042f279
Matthew D Jones <matthew.d.jones@tessella.com>
2016-08-22 13:49:43 +0100

    Re #5 update test file to cover the cases of int32 and int64 sample environment values

--- data/reduce_hdf5_file.py
+++ data/reduce_hdf5_file.py
@@ -1,4 +1,5 @@
 import h5py
+import numpy as np
 
 """
 Read data from compressed nexus file and write it to a new uncompressed one.
@@ -69,6 +70,17 @@ with h5py.File(compressed_file, 'r') as f_read:
         with h5py.File(uncompressed_file, 'r+') as f_write:
             f_write[time_dataset] = data_time[...]
             f_write[value_dataset] = data_value[...]
+
+    # Add fake int and long SE event data for using in unit tests
+    with h5py.File(uncompressed_file, 'r+') as f_write:
+        group_fake_int = f_write.get('/raw_data_1/selog').create_group('fake_int')
+        group_fake_int_values = group_fake_int.create_group('value_log')
+        group_fake_int_values.create_dataset('time', data=np.array([5.67, 23.5]).astype(np.float32))
+        group_fake_int_values.create_dataset('value', data=np.array([1, 2]).astype(np.int32))
+        group_fake_long = f_write.get('/raw_data_1/selog').create_group('fake_long')
+        group_fake_long_values = group_fake_long.create_group('value_log')
+        group_fake_long_values.create_dataset('time', data=np.array([5.67, 23.5]).astype(np.float32))
+        group_fake_long_values.create_dataset('value', data=np.array([7, 42]).astype(np.int64))
 
     with h5py.File(uncompressed_file, 'r+') as f_write:
         f_write['/raw_data_1/good_frames'] = max_frame
