84889fbbb29334ef5ab169106cf09da2f3d43de3
Moore <lamar.moore@stfc.ac.uk>
2017-01-16 15:00:51 +0000

    producer codebase not uses external third-party library

--- cmake/FindHDF5.cmake
+++ cmake/FindHDF5.cmake
@@ -4,29 +4,29 @@ find_path(HDF5_ROOT_DIR
         )   
 find_path(HDF5_DIR
         NAMES hdf5.h
-        HINTS third-party/include
+        HINTS ${THIRD_PARTY_DIR}/include
         )
 find_path(HDF5_INCLUDE_DIRS
         NAMES hdf5.h
-        HINTS third-party/include
+        HINTS ${THIRD_PARTY_DIR}/include
         )
 find_library(HDF5
         NAMES hdf5
-        HINTS third-party/lib
+        HINTS ${THIRD_PARTY_DIR}/lib
         )
 find_library(HDF5_DEBUG
         NAMES hdf5_D
-        HINTS third-party/lib
+        HINTS ${THIRD_PARTY_DIR}/lib
         )
         
 find_library(HDF5_CXX
         NAMES hdf5_cpp 
-        HINTS third-party/lib
+        HINTS ${THIRD_PARTY_DIR}/lib
         )
         
 find_library(HDF5_CXX_DEBUG
         NAMES hdf5_cpp_D
-        HINTS third-party/lib
+        HINTS ${THIRD_PARTY_DIR}/lib
         )
 
 if(HDF5_CXX_DEBUG)
