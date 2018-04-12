3a26005c6d117d93c990f7848088e1e5e51662b6
Matthew D Jones <matthew.d.jones@tessella.com>
2018-04-12 15:20:42 +0100

    Re #1 Add option to specify conan profile to use

--- unitTestRunner.cpp
+++ unitTestRunner.cpp
@@ -4,7 +4,7 @@ std::string testDataPath;
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
-  assert(argc == 2);
-  testDataPath = argv[1];
+  if (argc > 1)
+    testDataPath = argv[1];
   return RUN_ALL_TESTS();
 }
