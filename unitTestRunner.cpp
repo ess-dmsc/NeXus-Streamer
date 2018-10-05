8f72eedc7089dbbea23a2888604fd32af1db55b5
Jack Harper <jack.harper@stfc.ac.uk>
2018-10-05 11:18:30 +0100

    re #28 adding error for when data file path is not given

--- unitTestRunner.cpp
+++ unitTestRunner.cpp
@@ -4,7 +4,10 @@ std::string testDataPath;
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
-  if (argc > 1)
+  if (argc > 1) {
     testDataPath = argv[1];
+  } else {
+    throw std::runtime_error("No data file path given");
+  }
   return RUN_ALL_TESTS();
 }
