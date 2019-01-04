e9a04d763d178fc26e662236420165794e01c753
Matthew D Jones <matthew.d.jones@tessella.com>
2019-01-04 13:00:37 +0000

    #78 Add spdlog dependency

--- nexus_producer/src/main.cpp
+++ nexus_producer/src/main.cpp
@@ -5,6 +5,8 @@
 #include <CLI/CLI.hpp>
 #include <chrono>
 #include <iostream>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 #include <thread>
 
 uint64_t getTimeNowNanosecondsFromEpoch() {
@@ -83,6 +85,8 @@ int main(int argc, char **argv) {
                  false);
 
   CLI11_PARSE(App, argc, argv);
+  auto Logger = spdlog::stderr_color_mt("LOG");
+  Logger->debug("Application launched");
 
   auto detectorNumbers = getDetectorNumbers(settings);
   auto runStartTime = getTimeNowNanosecondsFromEpoch();
