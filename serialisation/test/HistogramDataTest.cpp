bddbffa6bdc40faafd0f9016fe966f1732bf822a
Matthew D Jones <matthew.d.jones@tessella.com>
2019-02-13 11:33:43 +0000

    #68 Test for correct file identifier in histogram data message

--- serialisation/test/HistogramDataTest.cpp
+++ serialisation/test/HistogramDataTest.cpp
@@ -1,10 +1,19 @@
-#include "HistogramData.h"
 #include "../../core/include/Message.h"
+#include "HistogramData.h"
 #include <gtest/gtest.h>
 
 class HistogramDataTest : public ::testing::Test {};
 
-TEST(HistogramDataTest, expect_createHistogramMessage_to_return_a_message_of_nonzero_size) {
+TEST(HistogramDataTest,
+     expect_createHistogramMessage_to_return_a_message_with_hs00_identifier) {
   auto message = createHistogramMessage({1, 2, 3}, {1, 1, 3}, {1.0, 3.0}, 0);
-  EXPECT_GT(message.size(), 0);
+
+  EXPECT_GT(message.size(), 8) << "Expected message size to at least be large "
+                                  "enough to contain the file identifier";
+
+  // Flatbuffer file identifier is in chars 4 to 7 of the data buffer
+  EXPECT_EQ(message.data()[4], 'h');
+  EXPECT_EQ(message.data()[5], 's');
+  EXPECT_EQ(message.data()[6], '0');
+  EXPECT_EQ(message.data()[7], '0');
 }
