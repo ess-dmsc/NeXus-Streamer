efcc21e0085e713816ba250812c7494f7f40a077
Matthew D Jones <matthew.d.jones@tessella.com>
2019-07-25 11:49:30 +0100

    #34 Add tests for validating json

--- nexus_producer/test/JSONDescriptionLoaderTest.cpp
+++ nexus_producer/test/JSONDescriptionLoaderTest.cpp
@@ -8,3 +8,13 @@ TEST(JSONDescriptionLoaderTest, test_JsonDescriptionIsValid_reports_false_for_em
   std::string emptyString;
   ASSERT_EQ(JsonDescriptionIsValid(emptyString), false);
 }
+
+TEST(JSONDescriptionLoaderTest, test_JsonDescriptionIsValid_reports_false_for_invalid_json) {
+  std::string invalidJson = R"(["field" : 123])";
+  ASSERT_EQ(JsonDescriptionIsValid(invalidJson), false);
+}
+
+TEST(JSONDescriptionLoaderTest, test_JsonDescriptionIsValid_reports_true_for_valid_json) {
+  std::string validJson = R"({"field" : 123})";
+  ASSERT_EQ(JsonDescriptionIsValid(validJson), true);
+}
