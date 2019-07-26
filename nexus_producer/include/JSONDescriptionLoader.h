5083410a53d97f80a33db309ec04d5aa9c0d245b
Matthew D Jones <matthew.d.jones@tessella.com>
2019-07-26 11:44:30 +0100

    #34 Add impl for string text replace

--- nexus_producer/include/JSONDescriptionLoader.h
+++ nexus_producer/include/JSONDescriptionLoader.h
@@ -6,5 +6,6 @@ namespace JSONDescriptionLoader {
 
 void checkIsValidJson(const std::string &textForValidation);
 std::string loadFromFile(const std::string &filepath);
-
+void replaceString(std::string &input, const std::string &search,
+                   const std::string &replace);
 }
--- nexus_producer/src/JSONDescriptionLoader.cpp
+++ nexus_producer/src/JSONDescriptionLoader.cpp
@@ -9,6 +9,15 @@ void checkIsValidJson(const std::string &textForValidation) {
   auto jsonObject = nlohmann::json::parse(textForValidation);
 }
 
+void replaceString(std::string &input, const std::string &search,
+                   const std::string &replace) {
+  size_t position = 0;
+  while ((position = input.find(search, position)) != std::string::npos) {
+    input.replace(position, search.length(), replace);
+    position += replace.length();
+  }
+}
+
 std::string loadFromFile(const std::string &filepath) {
   std::ifstream filestream(filepath);
   std::string json_string;
