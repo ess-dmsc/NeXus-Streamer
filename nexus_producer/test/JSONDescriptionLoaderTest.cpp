0d9d83038bc54d37107dfbb6a90ad118139997fb
Matthew D Jones <matthew.d.jones@tessella.com>
2019-07-26 11:44:50 +0100

    #34 Formatting

--- nexus_producer/test/JSONDescriptionLoaderTest.cpp
+++ nexus_producer/test/JSONDescriptionLoaderTest.cpp
@@ -7,12 +7,14 @@ class JSONDescriptionLoaderTest : public ::testing::Test {};
 
 TEST(JSONDescriptionLoaderTest, isValidJson_reports_false_for_empty_string) {
   std::string emptyString;
-  ASSERT_THROW(JSONDescriptionLoader::checkIsValidJson(emptyString), nlohmann::json::parse_error);
+  ASSERT_THROW(JSONDescriptionLoader::checkIsValidJson(emptyString),
+               nlohmann::json::parse_error);
 }
 
 TEST(JSONDescriptionLoaderTest, isValidJson_reports_false_for_invalid_json) {
-  std::string invalidJson = R"(["field" : 123])";  // invalid as no braces
-  ASSERT_THROW(JSONDescriptionLoader::checkIsValidJson(invalidJson), nlohmann::json::parse_error);
+  std::string invalidJson = R"(["field" : 123])"; // invalid as no braces
+  ASSERT_THROW(JSONDescriptionLoader::checkIsValidJson(invalidJson),
+               nlohmann::json::parse_error);
 }
 
 TEST(JSONDescriptionLoaderTest, isValidJson_reports_true_for_valid_json) {
@@ -20,7 +22,7 @@ TEST(JSONDescriptionLoaderTest, isValidJson_reports_true_for_valid_json) {
   ASSERT_NO_THROW(JSONDescriptionLoader::checkIsValidJson(validJson));
 }
 
-TEST(JSONDescriptionLoaderTest, replaceString_replaces_text_in_single_line_string) {
+TEST(JSONDescriptionLoaderTest, replaceString_replaces_text_in_string) {
   std::string inputString = "Hello all";
   JSONDescriptionLoader::replaceString(inputString, "all", "world!");
   ASSERT_EQ(inputString, "Hello world!");
